414fa6ede118dff7589388ac830938d3457ad595
Ethan <ethanlee515@gmail.com>
2019-11-20 23:36:40 +0800

    Remove unnecessary parameter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         try {
-            String bot_token = Files.readString(Path.of("bot-token.txt"), StandardCharsets.UTF_8);
+            String bot_token = Files.readString(Path.of("bot-token.txt"));
             System.out.println(bot_token);
         } catch(IOException e) {
             System.err.println("Can't open bot-token.txt.");
