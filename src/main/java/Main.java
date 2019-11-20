298fadb477000ceeb9ca1d858be0d6e250c21fc9
Ethan <ethanlee515@gmail.com>
2019-11-20 23:35:53 +0800

    Get file reading to work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world 2");
-        System.out.println("Hello world 3");
-        System.out.println("Hello world 4");
+        try {
+            String bot_token = Files.readString(Path.of("bot-token.txt"), StandardCharsets.UTF_8);
+            System.out.println(bot_token);
+        } catch(IOException e) {
+            System.err.println("Can't open bot-token.txt.");
+        }
     }
 }
